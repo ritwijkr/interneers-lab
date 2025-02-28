c373212951a8eefea75be1f0d836a846daac32d4
Ritwij Kumar <k.ritwij@iitg.ac.in>
2025-03-01 04:59:29 +0530

    Created a new API which takes in 3 parameters name, age and gender of a person and verifies if age and gender are valid

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -3,15 +3,51 @@ from django.urls import path
 from django.http import JsonResponse
 
 def hello_name(request):
+    
     """
-    A simple view that returns 'Hello, {name}' in JSON format.
-    Uses a query parameter named 'name'.
+    API that returns a greeting message with a name.
+    Supports additional parameters:
+    - 'greeting' (optional): Custom greeting message
     """
     # Get 'name' from the query string, default to 'World' if missing
     name = request.GET.get("name", "World")
-    return JsonResponse({"message": f"Hello, {name}!"})
+
+    message=f'hello, {name}'
+    return JsonResponse({"message": message})
+
+def condition1(age):
+    if not age.isdigit():
+        return True
+    else:
+        return False
+def condition2(gender):
+    if gender!='Female' and gender!='Male' and gender!="Other":
+        return True
+    else:
+        return False
+
+
+def introduction(request):
+    """
+    API that returns information about a individual.
+    name, age, gender (let's say)
+    """
+    name=request.GET.get("name","Raj")#setting ritwij as default
+    age=request.GET.get("age","19")
+    gender=request.GET.get("gender","Other")
+    if condition1(age) and condition2(gender):
+        return JsonResponse({"error": "Invalid age and gender."}, status=400)
+    elif condition1(age):
+        return JsonResponse({"error": "Invalid age."}, status=400)
+    elif condition2(gender):
+        return JsonResponse({"error": "Invalid gender."}, status=400)
+    
+    message=f'name: {name}, age: {age}, gender: {gender}'
+
+    return JsonResponse({"message": message})
+    
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_name),
+    path('hello/', introduction),
 ]
